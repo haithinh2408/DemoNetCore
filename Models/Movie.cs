4d3340dda2f977b239514fe3d7809352669164fb
Thinh Hai <haithinh23082000@gmail.com>
2021-09-27 08:59:35 +0700

    first commit

---
+++ Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is not blank")]
+        [MaxLength(20)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
